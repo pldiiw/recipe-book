580bb0a473c46146c8841a4966cd64ce3c632250
PLD <pldiiw@gmail.com>
2016-05-24 23:52:07 +0200

    Change build process and do a bit of styling
    The book's files are now compiled into one big html file that can also
    be converted to pdf using prince (http://www.princexml.com/).
    Pug files have also been renamed.

--- README.md
+++ README.md
@@ -11,15 +11,22 @@ All recipes taken from these french websites :
 
 ## How can I read it ?
 
-Be sure to have Nodejs v6.x installed and run the following commands :
+Be sure to have Nodejs v6.x installed and run the following commands:
 
     $ git clone https://github.com/pldiiw/recipe-book.git
     $ cd recipe-book
     $ npm i
+
+And one of these:
+
     $ npm start # if you have firefox installed
     $ npm run build # if you don't have firefox installed, afterwards open
                       build/index.html file with your favorite web browser
+    $ npm start-pdf # if you want the pdf and you're on linux
+                      Requires prince to be installed: http://www.princexml.com/
+    $ npm build-with-pdf # if you're not on linux (also requires prince) - the
+                           built pdf can be found in the build folder
 
 ## LICENSE
 
-This project is copyleft, so do everything you wish with it.
+This project is under the unlicense, so do everything you wish with it.
--- build-book.js
+++ build-book.js
@@ -6,19 +6,7 @@ const pug = require('pug');
 const md = require('markdown-it')();
 const stylus = require('stylus');
 
-const index =
-'doctype html\n' +
-'html\n' +
-'  head\n' +
-'    script.\n' +
-'      location = "book/cover-front.html";\n' +
-'  body';
-
-fs.writeFileSync(
-  'build/index.html',
-  pug.render(index),
-  'utf8'
-);
+const secSep = '\n<!--###################################################-->\n';
 
 fs.readFile('styles.styl', 'utf8', (err, content) => {
   fs.writeFile('build/styles.css', stylus.render(content), 'utf8', (err) => {
@@ -26,15 +14,13 @@ fs.readFile('styles.styl', 'utf8', (err, content) => {
   });
 });
 
+let book = '';
+
 [
-  'cover-front.pug',
-  'first-page.pug'
+  'frontcover.pug',
+  'dedication.pug'
 ].forEach((file) => {
-  fs.writeFileSync(
-    'build/book/' + path.basename(file, '.pug') + ".html",
-    pug.renderFile('book/' + file, { pretty: true }),
-    'utf8'
-  );
+  book += pug.renderFile('book/' + file, { pretty: true }) + secSep;
 });
 
 const recipes = fs.readdirSync('book/recipes/');
@@ -42,21 +28,19 @@ const recipes = fs.readdirSync('book/recipes/');
 const recipesTitles = recipes
   .map((file) => fs.readFileSync('book/recipes/' + file, 'utf8').split('\n')[0].slice(2));
 
-fs.writeFileSync(
-  'build/book/table-of-contents.html',
-  pug.renderFile(
-    'book/table-of-contents.pug',
-    { recipesTitles: recipesTitles, pretty: true }
-  ),
-  'utf8'
-);
+book += pug.renderFile('book/toc.pug',
+                       { recipesTitles: recipesTitles, pretty: true }
+                      ) + secSep;
 
 const layoutFn = pug.compileFile('book/layout.pug', { pretty: true });
 recipes.forEach((file) => {
-  fs.writeFile(
-    'build/book/recipes/' + path.basename(file, '.md') + '.html',
-    layoutFn({
-      recipe: md.render(fs.readFileSync('book/recipes/' + file, 'utf8'))
-    })
-  );
+  book += layoutFn({
+    recipe: md.render(fs.readFileSync('book/recipes/' + file, 'utf8'))
+  }) + secSep;
 });
+
+fs.writeFileSync(
+  'build/index.html',
+  pug.renderFile('book/index.pug', {book: book, pretty: true}), 
+  'utf8'
+);
